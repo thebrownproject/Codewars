d00eb8a95a4a2e3fff243ea8274582188b715696
thebrownproject <fraserbrown@live.com>
2025-01-20 21:26:56 +1100

    Codewars

--- 8kyu/grasshopper_summation.js
+++ 8kyu/grasshopper_summation.js
@@ -12,10 +12,10 @@
 
 var summation = function (num) {
   let total = 0;
-  for (i = 0; i < num; i++) {
+  for (i = 0; i <= num; i++) {
     total += i;
   }
   return total;
 };
 
-console.log(summation(6));
+console.log(summation(8));
