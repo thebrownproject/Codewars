aa02792e19c3194c0e8fc03ddb848d1e94c27efd
thebrownproject <fraserbrown@live.com>
2025-01-21 21:44:38 +1100

    Codewars

---
+++ 8kyu/grasshopper_grade_book.js
@@ -0,0 +1,29 @@
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+function getGrade(s1, s2, s3) {
+  grade = (s1 + s2 + s3) / 3;
+  if (grade >= 100) {
+    return "A";
+  } else if (grade >= 80) {
+    return "B";
+  } else if (grade >= 70) {
+    return "C";
+  } else if (grade >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+console.log((90 + 90 + 90) / 3);
+
+console.log(getGrade(95, 90, 93));
